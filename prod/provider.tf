c232f7b460bd224abe99fce1e92b6df07a00dbaa
abhishek095859 <abhishek.narmula@minfytech.com>
2026-03-11 11:56:42 +0530

    Update provider.tf

--- prod/provider.tf
+++ prod/provider.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket = "athena-apollotyres-test"
-    key    = "infra"
+    bucket = "my-terraform-tftate"
+    key    = "infra/terraform.tfstate"
     region = "ap-south-1"
         assume_role = {
       role_arn = "arn:aws:iam::602151476946:role/CrossAccountStateRole"
